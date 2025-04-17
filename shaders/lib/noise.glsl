bd6d5f27b082cfeb4c31568248b7bc82b634a7b7
Marcel Alexandru Nitan <nitan.marcel@protonmail.com>
2025-04-18 00:51:59 +0300

    Oops. Add missing curlNoise function

--- shaders/lib/noise.glsl
+++ shaders/lib/noise.glsl
@@ -28,3 +28,20 @@ float noise(vec3 p) {
     
     return mix(y0, y1, u.z);
 }
+
+vec3 curlNoise(vec3 p) {
+    const float epsilon = 0.0001;
+    vec3 dx = vec3(epsilon, 0.0, 0.0);
+    vec3 dy = vec3(0.0, epsilon, 0.0);
+    vec3 dz = vec3(0.0, 0.0, epsilon);
+    
+    float x1 = noise(p + dx) - noise(p - dx);
+    float y1 = noise(p + dy) - noise(p - dy);
+    float z1 = noise(p + dz) - noise(p - dz);
+    
+    float x2 = noise(p + dx) - noise(p - dx);
+    float y2 = noise(p + dy) - noise(p - dy);
+    float z2 = noise(p + dz) - noise(p - dz);
+    
+    return normalize(vec3(y2 - z1, z2 - x1, x2 - y1));
+}
