aeb2272a32ab11a101af31d8dfae1ae5590decf2
Marcel Alexandru Nitan <nitan.marcel@protonmail.com>
2025-04-21 18:03:41 +0300

    Bloom

--- shaders/program/composite.glsl
+++ shaders/program/composite.glsl
@@ -19,12 +19,14 @@ void main() {
 uniform sampler2D colortex0;
 
 in vec2 texcoord;
+uniform sampler2D colortex1;
 
 /* RENDERTARGETS: 0 */
 layout(location = 0) out vec4 color;
 
 void main() {
 	color = texture(colortex0, texcoord);
+
 	color.rgb = pow(color.rgb, vec3(2.2));
 
 	color.rgb = pow(color.rgb, vec3(1.0/GAMMA));
@@ -33,6 +35,20 @@ void main() {
 	hsv.y *= SATURATION;
 
 	color.rgb = hsv2rgb(hsv);
+
+	vec3 blur = texture2D(colortex1, texcoord / 4.0).rgb;
+	blur = clamp(blur, vec3(0.0), vec3(1.0));
+	blur *= blur;
+
+	float luminance = dot(blur, vec3(0.2126, 0.7152, 0.0722));
+	float threshold = 0.7;
+	float softThreshold = 0.1;
+
+	float brightness = smoothstep(threshold - softThreshold, threshold + softThreshold, luminance);
+	blur *= brightness;
+
+	float bloomStrength = BLOOM_STRENGTH * 0.08;
+	color.rgb = mix(color.rgb, blur, bloomStrength);
 
 	color.rgb = unchartedTonemapping(color.rgb * EXPOSURE);
 	color.rgb = pow(color.rgb, vec3(1/2.2));
