bf158bbb4c34e8a66057f87d4e477189c936002a
Marcel Alexandru Nitan <nitan.marcel@protonmail.com>
2025-04-23 17:16:37 +0300

    Properly fix indirect lighting for hands/particles

--- shaders/program/gbuffers_basic.glsl
+++ shaders/program/gbuffers_basic.glsl
@@ -56,37 +56,33 @@ uniform vec3 cameraPosition;
 layout(location = 0) out vec4 color;
 
 float calculateLightingFactor(vec3 worldPos, vec3 viewDir) {
-    #ifndef DISABLE_INDIRECT_LIGHTING
-        vec3 shadowLightDirection = normalize(mat3(gbufferModelViewInverse) * shadowLightPosition);
+    vec3 shadowLightDirection = normalize(mat3(gbufferModelViewInverse) * shadowLightPosition);
 
-        vec4 normalData = texture(normals, texcoord)*2.0-1.0;
+    vec4 normalData = texture(normals, texcoord)*2.0-1.0;
 
-        vec3 normalNormal = vec3(normalData.xy, sqrt(1.0 - dot(normalData.xy, normalData.xy))*2.0-1.0);
+    vec3 normalNormal = vec3(normalData.xy, sqrt(1.0 - dot(normalData.xy, normalData.xy))*2.0-1.0);
 
-        vec3 normalWorld = TBN * normalNormal;
-        
-        vec4 specularData = texture(specular, texcoord);
+    vec3 normalWorld = TBN * normalNormal;
+    
+    vec4 specularData = texture(specular, texcoord);
 
-        float perceptualSmoothness = specularData.r;
+    float perceptualSmoothness = specularData.r;
 
-        float roughness = pow(1.0 - perceptualSmoothness, 2.0);
+    float roughness = pow(1.0 - perceptualSmoothness, 2.0);
 
-        float smoothness = 1-roughness;
+    float smoothness = 1-roughness;
 
-        vec3 reflectionDir = reflect(-shadowLightDirection, normalWorld);
-        
-        float diffuseLight = roughness * clamp(dot(shadowLightDirection, normalWorld), 0.0,1.0);
+    vec3 reflectionDir = reflect(-shadowLightDirection, normalWorld);
+    
+    float diffuseLight = roughness * clamp(dot(shadowLightDirection, normalWorld), 0.0,1.0);
 
-        float shininess = (1+(smoothness) * 100);
+    float shininess = (1+(smoothness) * 100);
 
-        float specularLight = clamp(smoothness * pow(dot(reflectionDir, viewDir), shininess), 0.0, 1.0);
+    float specularLight = clamp(smoothness * pow(dot(reflectionDir, viewDir), shininess), 0.0, 1.0);
 
-        float ambientLight = 0.2;
+    float ambientLight = 0.2;
 
-        return ambientLight + diffuseLight + specularLight;
-    #else
-        return 1.0;
-    #endif
+    return ambientLight + diffuseLight + specularLight;
 }
 
 void main() {
@@ -105,6 +101,6 @@ void main() {
     vec3 viewDir = normalize(cameraPosition - worldPos);
 
     float lightBrightness = calculateLightingFactor(worldPos, viewDir);
-    color *= lightBrightness;
+    color.rgb *= lightBrightness;
 }
 #endif // FRAGMENT_SHADER
